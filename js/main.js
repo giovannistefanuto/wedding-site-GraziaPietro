b3140489454114574ee88f22b24f7015ccdf4654
Giovanni Stefanuto <gio503ste@gmail.com>
2025-01-02 11:16:36 +0100

    update

--- js/main.js
+++ js/main.js
@@ -2,20 +2,17 @@
     "use strict";
 
     // Spinner
-var spinner = function () {
-    setTimeout(function () {
-        if ($('#spinner').length > 0) {
-            $('#spinner').removeClass('show');
-        }
-    }, 2000); // 2 secondi di visibilità
-};
-spinner();
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
 
-    
-    
     // Initiate the wowjs
     new WOW().init();
-
 
     // Fixed Navbar
     $(window).scroll(function () {
@@ -26,36 +23,38 @@ spinner();
         }
     });
 
-
     // Smooth scrolling on the navbar links
     $(".navbar-nav a").on('click', function (event) {
         if (this.hash !== "") {
             event.preventDefault();
-            
+
             $('html, body').animate({
                 scrollTop: $(this.hash).offset().top - 90
             }, 1500, 'easeInOutExpo');
-            
+
             if ($(this).parents('.navbar-nav').length) {
                 $('.navbar-nav .active').removeClass('active');
                 $(this).closest('a').addClass('active');
             }
         }
     });
-    
-    
-   // Back to top button
-   $(window).scroll(function () {
-    if ($(this).scrollTop() > 300) {
-        $('.back-to-top').fadeIn('slow');
-    } else {
-        $('.back-to-top').fadeOut('slow');
-    }
+
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
         return false;
-    }); 
+    });
+
+    // Aggiungi qui il nuovo evento per rimuovere lo spinner al caricamento
+    $(window).on('load', function () {
+        $('#spinner').removeClass('show');
+    });
 
 })(jQuery);
-
